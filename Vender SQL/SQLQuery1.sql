f717b89e5f5a1baaec9e743193e377d6dd6cc981
developeruzb <inoyatulla.work@gmail.com>
2023-05-08 02:01:56 +0500

    set file

---
+++ Vender SQL/SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿SELECT  SUM(Total) AS Total FROM Bill Where;
+
+SELECT COUNT(Name) AS total from Category;
+
+
+SELECT  SUM(Quantity) AS Total FROM Product;
+
+
+SELECT Quantity FROM Product Where Name="Apple";
